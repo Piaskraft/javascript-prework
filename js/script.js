5baa0ad2f37bad794803aa9be14e5fd418953ae6
Mateusz Piasecki <piasecki.mateusz@interia.pl>
2025-05-02 12:24:46 +0200

    Task 4.5 – Refactor game logic using functions: getMoveName and displayResult

--- js/script.js
+++ js/script.js
@@ -1,40 +1,48 @@
+// Funkcja zamieniająca numer ruchu na jego nazwę
+function getMoveName(argMoveId) {
+    if (argMoveId == 1) {
+        return 'kamień';
+    } else if (argMoveId == 2) {
+        return 'papier';
+    } else if (argMoveId == 3) {
+        return 'nożyce';
+    } else {
+        printMessage('Nie znam ruchu o id ' + argMoveId);
+        return 'nieznany ruch';
+    }
+}
+
+// Funkcja wyświetlająca wynik gry
+function displayResult(argComputerMove, argPlayerMove) {
+    printMessage('Komputer zagrał: ' + argComputerMove);
+    printMessage('Gracz zagrał: ' + argPlayerMove);
+
+    if (argComputerMove == argPlayerMove) {
+        printMessage('Remis!');
+    } else if (
+        (argComputerMove == 'kamień' && argPlayerMove == 'papier') ||
+        (argComputerMove == 'papier' && argPlayerMove == 'nożyce') ||
+        (argComputerMove == 'nożyce' && argPlayerMove == 'kamień')
+    ) {
+        printMessage('Ty wygrywasz!');
+    } else if (argPlayerMove == 'nieznany ruch') {
+        printMessage('Nieznany ruch gracza!');
+    } else {
+        printMessage('Komputer wygrywa!');
+    }
+}
+
+// Wylosowanie ruchu komputera (1-3)
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba dla komputera:', randomNumber);
 
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
 
-if (randomNumber == 1) {
-    computerMove = 'kamień';
-} else if (randomNumber == 2) {
-    computerMove = 'papier';
-} else if (randomNumber == 3) {
-    computerMove = 'nożyce';
-}
+// Pytamy gracza o wybór (1-3)
 let playerInput = prompt('Wybierz swój ruch: 1 - kamień, 2 - papier, 3 - nożyce');
 console.log('Gracz wpisał:', playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(playerInput);
 
-if (playerInput == '1') {
-    playerMove = 'kamień';
-} else if (playerInput == '2') {
-    playerMove = 'papier';
-} else if (playerInput == '3') {
-    playerMove = 'nożyce';
-}
-printMessage('Komputer zagrał: ' + computerMove);
-printMessage('Gracz zagrał: ' + playerMove);
-
-if (computerMove == playerMove) {
-    printMessage('Remis!');
-} else if (
-    (computerMove == 'kamień' && playerMove == 'papier') ||
-    (computerMove == 'papier' && playerMove == 'nożyce') ||
-    (computerMove == 'nożyce' && playerMove == 'kamień')
-) {
-    printMessage('Ty wygrywasz!');
-} else if (playerMove == 'nieznany ruch') {
-    printMessage('Nieznany ruch gracza!');
-} else {
-    printMessage('Komputer wygrywa!');
-}
+// Wyświetlenie wyniku gry
+displayResult(computerMove, playerMove);
